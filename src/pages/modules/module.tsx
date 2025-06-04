64589831016d661c8b7e54590670728db5db6269
Haptic Labs <205575443+hapticlabsdev@users.noreply.github.com>
2025-06-03 22:14:21 -0600

    fix(HAP-12): various fixes

--- src/pages/modules/module.tsx
+++ src/pages/modules/module.tsx
@@ -2,14 +2,15 @@ import { Navigate, useParams } from 'react-router-dom';
 import { ModuleSourcesGrid } from '../../components';
 import { ModuleActionsList } from '../../components/actions';
 import { ModuleOutputsGrid } from '../../components/outputs';
-import { Center, Group, Loader, Stack, Title } from '@mantine/core';
+import { Button, Center, Group, Loader, Stack, Title } from '@mantine/core';
 import { ModulesQueries } from '../../queries';
 import { useAuth } from '../../providers';
 import { ModuleScheduleButton } from '../../components/schedule';
+import { useState } from 'react';
 
 export const ModulePage = () => {
   const { moduleId } = useParams();
-  const { user } = useAuth();
+  const { user, supabaseClient } = useAuth();
   const { data: moduleName, isLoading } = ModulesQueries.useUserModulesQuery(
     {
       userId: user?.id ?? '',
@@ -19,6 +20,18 @@ export const ModulePage = () => {
       select: (data) => data.find((module) => module.id === moduleId)?.name,
     },
   );
+  const [isRunning, setIsRunning] = useState(false); // TODO: use supabase realtime to track running status
+
+  const handleRun = async () => {
+    setIsRunning(true);
+    try {
+      await supabaseClient.functions.invoke(`/modules/${moduleId}/run`, {
+        method: 'POST',
+      });
+    } finally {
+      setIsRunning(false);
+    }
+  };
 
   if (!moduleId) return <Navigate to='/404' />;
 
@@ -39,7 +52,12 @@ export const ModulePage = () => {
     >
       <Group align='end' justify='space-between' mih={65}>
         <Title>{moduleName ?? ''}</Title>
-        <ModuleScheduleButton moduleId={moduleId} />
+        <Group>
+          <Button onClick={handleRun} loading={isRunning}>
+            Run!
+          </Button>
+          <ModuleScheduleButton moduleId={moduleId} />
+        </Group>
       </Group>
       <ModuleSourcesGrid moduleId={moduleId} />
       <ModuleActionsList moduleId={moduleId} />
